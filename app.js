4ec269a250c7ccef76ab92d87e42b709c0295a4a
BabySailor <abedanieloluwatomide@gmail.com>
2025-02-10 19:51:32 +0100

    Made some changes

--- app.js
+++ app.js
@@ -1,81 +1,58 @@
-//Requiring modules
-require('dotenv').config();
+require("dotenv").config();
 
-//requiring express (so we can create a express server )
-const express = require('express');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
+const connectDB = require("./server/config/db");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
 
-//requiring express-ejs-layouts(it is essentially for creating layouts in our app)
-const expressLayouts = require('express-ejs-layouts');
-
-const methodOverride = require('method-override');
-
-//requiring mongoose (so we can connect to our database)
-const connectDB = require('./server/config/db');
-
-//requiring session
-const session = require('express-session');
-
-//requiring passport
-const passport = require('passport');
-
-//requring Mongostore
-const MongoStore = require('connect-mongo');
-
-//creating a new express app
 const app = express();
+const port = process.env.PORT || 8000;
 
-//Giving it a port number
-const port = 8000 || process.env.PORT;
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.use(express.static(__dirname + "/public"));
 
+// Database connection
+connectDB();
+
+// Session
 app.use(
   session({
-    secret: 'Call of Duty',
+    secret: "Call of Duty",
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
       mongoUrl: process.env.MONGO_URI,
     }),
-    cookie: { Date: new Date(Date.now() + 3600000) },
+    cookie: { maxAge: 3600000, secure: process.env.NODE_ENV === "production" },
   })
 );
 
-//Passport config
+// Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-//ADDING MIDDLEWARES
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); //This is used to parse the data from page to page
-app.use(methodOverride('_method'));
-app.use((req, res, next) => {
-  console.log("Request Method:", req.method);
-  console.log("Request URL:", req.originalUrl);
-  next();
+// Templating
+app.use(expressLayouts);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+// Routes
+app.use("/", require("./server/routes/index"));
+app.use("/", require("./server/routes/auth"));
+app.use("/", require("./server/routes/dashboard"));
+
+// 404 Handler
+app.get("*", (req, res) => {
+  res.status(404).render("404");
 });
 
-//Connecting to the database
-connectDB(); //This will connect to the database
-//STATIC FILES(middleware)
-app.use(express.static('public')); //This is used to serve static files like css,js,images etc(For linking)
-
-//Templating Engine
-app.use(expressLayouts); //This is used to create layouts in our app
-
-//Setting the defualt layouts
-app.set('layout', './layouts/main'); //setting the default layout to main
-
-app.set('view engine', 'ejs'); //setting the view engine to ejs
-
-//Routes
-app.use('/', require('./server/routes/index')); //linking the index.js file in routes folder
-app.use('/', require('./server/routes/auth')); //linking the auth.js file in routes folder
-app.use('/', require('./server/routes/dashboard')); //linking the Dashboard.js file in routes folder
-
-//HAndles 404
-app.get('*', (req, res) => {
-  res.status(404).render('404');
-});
-
+// Start server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`); //This will run when the server is started
+  console.log(`Server running on port ${port}`);
 });
