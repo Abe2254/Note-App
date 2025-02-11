63b9e09dd4c1a1aece12a2dffa1f2c1171e7c019
BabySailor <abedanieloluwatomide@gmail.com>
2025-02-11 18:52:46 +0100

    i would change it back

--- server/middleware/checkAuth.js
+++ server/middleware/checkAuth.js
@@ -1,9 +1,9 @@
-exports.isLoggedIn = function (req, res, next) {
-    console.log("Checking Auth: ", req.user); // Debugging
-    if (req.user) {
-      next();
-    } else {
-      return res.status(401).send('Access Denied');
-    }
-  };
+// exports.isLoggedIn = function (req, res, next) {
+//     console.log("Checking Auth: ", req.user); // Debugging
+//     if (req.user) {
+//       next();
+//     } else {
+//       return res.status(401).send('Access Denied');
+//     }
+//   };
   
